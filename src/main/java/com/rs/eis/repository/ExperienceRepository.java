f581ebde971080a3bd916cd218f5a2621f5af57d
divya <divya@LAPTOP-CIVP2820>
2020-05-19 08:06:10 +0530

    experience and technology

--- src/main/java/com/rs/eis/repository/ExperienceRepository.java
+++ src/main/java/com/rs/eis/repository/ExperienceRepository.java
@@ -1,7 +1,5 @@
 package com.rs.eis.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +7,5 @@ import com.rs.eis.model.Experience;
 
 @Repository
 public interface ExperienceRepository extends JpaRepository<Experience, Integer> {
-	//Optional<Experience> findById(int employeeid);
 
 }
