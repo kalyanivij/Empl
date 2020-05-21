0d4be8e05b4d5079734fa462cc7466b9775cc855
Rajae <SS@DELL>
2020-05-21 09:17:39 +0530

    Qualification table

--- src/main/java/com/rs/fer/validation/impl/ValidationUtilImpl.java
+++ src/main/java/com/rs/fer/validation/impl/ValidationUtilImpl.java
@@ -55,15 +55,7 @@ public class ValidationUtilImpl implements ValidationUtil {
 		return errorMessages;
 	}
 
-	@Override
-	public Set<String> validateGetExpensesRequest(int userId) {
-		Set<String> errorMessages = new HashSet<String>();
-
-		addError(errorMessages, userId, "Please enter valid userId");
-
-		return errorMessages;
-	}
-
+	
 	@Override
 	public Set<String> validateExpenseReportRequest(int userId, String type, String fromDate, String toDate) {
 		Set<String> errorMessages = new HashSet<String>();
