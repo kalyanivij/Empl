3b6c2d05930b0abbfc513603aa78f1af46f00487
G.ASHOK <G.ASHOK@LAPTOP-9339BM61>
2020-05-22 13:21:39 +0530

    Emp_awards

--- src/main/java/com/rs/eis/FERApplication.java
+++ src/main/java/com/rs/eis/FERApplication.java
@@ -10,10 +10,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class FERApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FERApplication.class, args);
-		
-		
+
 	}
-	
 	
 
 }
